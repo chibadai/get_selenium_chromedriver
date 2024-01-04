db3c6ab53895839e344f0544752d1e5a4d6e936e
Daisuke Chiba <92.ch.07.b.17.dai@gmail.com>
2024-01-04 15:31:19 +0900

    対象データの取得
    - 必要となるChromeバージョンから、ダウンロード可能なChromeDriverで近いデータを取得する

--- py/read-json.py
+++ py/read-json.py
@@ -1,5 +1,12 @@
 # Chrome Versionを取得しておく
-# command: dir /B /O-N "C:\Program Files\Google\Chrome\Application" | findstr ^[0-9].*
+# ChromeDriverのバージョンを取得する
+with open("./data/chrome_version.txt", "r") as f:
+    chrome_version = f.read()
+print(chrome_version)
+# chrome_version は最終行ので取得する
+now_chrome_version = chrome_version.split("\n")[-2]
+print(now_chrome_version)
+split_now_chrome_version = [int(x) for x in now_chrome_version.split(".")]
 
 ## 保存したファイルを読み込み利用可能な状態にする
 json_name = "known-good-versions-with-downloads.json"
@@ -29,6 +36,7 @@ for first_key, value in json_data.items():
 key_timestamp = "timestamp"
 # print(json_data[key_timestamp])
 key_versions = "versions"
+key_version = "version"
 # print(json_data[key_versions])
 # print(type(json_data[key_versions]))
 json_data_versions = json_data[key_versions]
@@ -45,7 +53,7 @@ for json_data_idx in range(0, len(json_data_versions)):
     """
     # for key_seconds, value in json_data_version.items():
     #     print(key_seconds, value)
-    key_version = "version"
+    # key_version = "version"
     key_revision = "revision"
     key_downloads = "downloads"
     json_data_version_version = json_data_version[key_version]
@@ -77,4 +85,27 @@ for json_data_idx in range(0, len(json_data_versions)):
     ] = json_data_version_downloads_chrome_dict["url"]
 # print(chrome_driver_version_dict)
 for json_data_idx in chrome_driver_version_dict.keys():
-    print(json_data_idx, chrome_driver_version_dict[json_data_idx])
+    # print(json_data_idx, chrome_driver_version_dict[json_data_idx])
+    version = chrome_driver_version_dict[json_data_idx][key_version]
+    split_version = [int(x) for x in version.split(".")]
+    # print(split_version, split_now_chrome_version)
+    if split_version[0] == split_now_chrome_version[0]:
+        # print(split_version, split_now_chrome_version)
+        if split_version[1] == split_now_chrome_version[1]:
+            # print(split_version, split_now_chrome_version)
+            if split_version[2] == split_now_chrome_version[2]:
+                print(split_version, split_now_chrome_version)
+                if split_version[3] >= split_now_chrome_version[3]:
+                    print(split_version[3], split_now_chrome_version[3])
+                    print("OK")
+                    break
+    if split_version[0] > split_now_chrome_version[0]:
+        break
+    # if version == now_chrome_version:
+    #     print("OK")
+    #     break
+
+# print(chrome_driver_version_dict[json_data_idx][key_version])
+print(chrome_driver_version_dict[json_data_idx - 1][key_version])
+print(chrome_driver_version_dict[json_data_idx - 1])
+# 上記のデータをダウンロードする
